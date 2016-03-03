1f2d902b08eba3284c4e5729785bce2da48c4858
takiy33 <takiy33@users.noreply.github.com>
2016-03-03 09:53:25 +0900

    Revert "Remove the code for Rspec2 conpatibility"
    This reverts commit f9fc0a47d9d21a85fc41a4cd278b7129ccb5a64c.
    
    I have deleted code by mistake.

--- lib/capybara_screenshot_idobata/rspec.rb
+++ lib/capybara_screenshot_idobata/rspec.rb
@@ -9,10 +9,12 @@ end
 
 CapybaraScreenshotIdobata.configure do |config|
   config.message_formatter = proc {|file, line, context|
+    example = context.respond_to?(:example) ? context.example : RSpec.current_example # For RSpec 2/3 conpatiblity
+
 <<-MESSAGE
 Screenshot from: <code>#{ERB::Util.h(file)}:#{line}</code>
 <br>
-<b>#{ERB::Util.h(RSpec.current_example.full_description)}</b>
+<b>#{ERB::Util.h(example.full_description)}</b>
 MESSAGE
   }
 end
